c0ec0186234accd5fcbdd9a6ccd93c71bb493a77
cian-oh <164069586+cian-oh@users.noreply.github.com>
2026-04-04 10:36:11 +0100

    Add files via upload

---
+++ hypertrack/config.js
@@ -0,0 +1,5 @@
+window.__HYPERTRACK_CONFIG__ = {
+  supabaseUrl: "https://oidnajdyidoycluouaqe.supabase.co",
+  supabaseAnonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pZG5hamR5aWRveWNsdW91YXFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyOTEzNTYsImV4cCI6MjA5MDg2NzM1Nn0.wxibTfLBCBtjU7IKgHey7etofKIA88R1mvWXvmJSVOY",
+};
